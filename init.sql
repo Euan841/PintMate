ff7f50bdaa065105bf59b679e38fc8de52f7712b
Ahzidol <ahzidol4@protonmail.com>
2024-06-04 20:36:59 +0100

    Added Pub Table to Init

--- init.sql
+++ init.sql
@@ -5,3 +5,9 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    pubName VARCHAR(50) NOT NULL,
+    pubRating INT NULL,
+);
